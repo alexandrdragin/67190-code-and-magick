479702473d9e55900e87f8a7ee74aecec47252fe
Alexandr Dragin <one.eastside@gmail.com>
2016-06-21 08:29:03 +0300

    все.  я сдаюсь

--- src/game.js
+++ src/game.js
@@ -783,50 +783,42 @@
   */
   var headerClouds = document.querySelector('.header-clouds');
   var seeClouds = true;
-
   var trottleDelay = 100;
-
-  function initScroll() {
-    window.addEventListener('scroll', function() {
-      trottle(checkVisbility(), trottleDelay);
-      moveClouds();
-    });
-  }
 
   function moveClouds() {
     if (seeClouds) {
-      console.log('seeClouds');
       var num = window.pageYOffset;
       headerClouds.style.top = num / 4 + 0 + 'px';
       headerClouds.style.backgroundPosition = num / 3 + 0 + 'px';
     }
   }
 
-  var checkVisbility = function() {
-    console.log('checkVisbility');
-    if (window.pageYOffset < 300) {
-      seeClouds = true;
-      game.setGameStatus(Game.Verdict.CONTINUE);
-    } else {
-      console.log('pause');
-      seeClouds = false;
-      game.setGameStatus(Game.Verdict.PAUSE);
-    }
-  };
-
   var trottle = function(funct, time) {
+    debugger;
     var lastCall = Date.now();
 
     var createFunct = function() {
       if (Date.now() - lastCall >= time) {
+        debugger;
         funct();
       }
       lastCall = Date.now();
     };
-    console.log('return');
     return createFunct();
   };
 
-  initScroll();
+  var setGamePause = trottle(function() {
+    debugger;
+    if (window.pageYOffset < 300) {
+      seeClouds = true;
+      game.setGameStatus(Game.Verdict.CONTINUE);
+    } else {
+      seeClouds = false;
+      game.setGameStatus(Game.Verdict.PAUSE);
+    }
+  }, trottleDelay);
+
+  window.addEventListener('scroll', moveClouds());
+  window.addEventListener('scroll', setGamePause);
 
 })();
